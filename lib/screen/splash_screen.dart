a266db77c971de39ccfce32aa083b2f5eced5b0a
k_terada <moonsocean@gmail.com>
2025-02-14 16:21:19 +0900

    [#4] add: SplashScreenを追加

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_training/screen/weather_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    _navigateToNextScreen();
+  }
+
+  Future<void> _navigateToNextScreen() async {
+    await SchedulerBinding.instance.endOfFrame;
+    await Future.delayed(Duration(microseconds: 500));
+    if (mounted) {
+      await Navigator.push(
+        context,
+        MaterialPageRoute<void>(
+          builder: (BuildContext context) {
+            return const WeatherScreen();
+          },
+        ),
+      );
+      unawaited(_navigateToNextScreen());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.green,
+      body: SizedBox.expand(),
+    );
+  }
+}
