8700ebf942914fe3da002ecedcc64226a5a492b1
k_terada <moonsocean@gmail.com>
2025-02-14 16:37:26 +0900

    [#4] fix: 警告に対応

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_training/screen/weather_screen.dart';
@@ -15,22 +16,22 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    _navigateToNextScreen();
+    unawaited(_navigateToWeatherScreen());
   }
 
-  Future<void> _navigateToNextScreen() async {
+  Future<void> _navigateToWeatherScreen() async {
     await SchedulerBinding.instance.endOfFrame;
-    await Future.delayed(const Duration(milliseconds: 500));
+    await Future<void>.delayed(const Duration(milliseconds: 500));
     if (mounted) {
       await Navigator.push(
         context,
         MaterialPageRoute<void>(
-          builder: (BuildContext context) {
+          builder: (context) {
             return const WeatherScreen();
           },
         ),
       );
-      unawaited(_navigateToNextScreen());
+      unawaited(_navigateToWeatherScreen());
     }
   }
 
