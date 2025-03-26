07c0d57550a21915db8ad4df1c64d99e54e03888
k_terada <moonsocean@gmail.com>
2025-03-26 16:43:49 +0900

    [#8] build.ymlを利用する形に修正

--- lib/model/weather_info.dart
+++ lib/model/weather_info.dart
@@ -7,28 +7,14 @@ enum WeatherCondition {
   sunny,
   cloudy,
   rainy;
-
-  factory WeatherCondition.fromString(String conditionString) {
-    switch (conditionString) {
-      case 'sunny':
-        return WeatherCondition.sunny;
-      case 'cloudy':
-        return WeatherCondition.cloudy;
-      case 'rainy':
-        return WeatherCondition.rainy;
-      default:
-        throw Exception('bad weather condition name $conditionString');
-    }
-  }
 }
 
 @freezed
-@JsonSerializable(fieldRename: FieldRename.snake)
 abstract class WeatherInfo with _$WeatherInfo {
   const factory WeatherInfo({
     required int maxTemperature,
     required int minTemperature,
-    required WeatherCondition condition,
+    required WeatherCondition weatherCondition,
   }) = _WeatherInfo;
 
   factory WeatherInfo.fromJson(Map<String, Object?> json) =>
