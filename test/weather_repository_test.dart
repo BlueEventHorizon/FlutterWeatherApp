8999725a211a40de420fa7b902ed08279145e81a
k_terada <moonsocean@gmail.com>
2025-04-23 16:12:52 +0900

    [#10] WeatherRepository Testをriviepodを利用して修正

--- test/weather_repository_test.dart
+++ test/weather_repository_test.dart
@@ -1,17 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_training/data/api/weather_api.dart';
 import 'package:flutter_training/data/repository/weather_repository.dart';
+import 'package:flutter_training/domain/exception/app_exception.dart';
 import 'package:flutter_training/domain/model/weather_info.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
+import 'helper/create_container.dart';
 @GenerateNiceMocks([MockSpec<WeatherAPI>()])
 import 'weather_repository_test.mocks.dart';
 
 void main() {
   test('this is test', () {
     final mock = MockWeatherAPI();
-    final api = WeatherRepository(weatherAPI: mock);
 
     const response = '''
     {
@@ -22,14 +23,64 @@ void main() {
     }
     ''';
 
+    final container = createContainer(
+      overrides: [
+        weatherAPIProvider.overrideWith((ref) {
+          return mock;
+        }),
+      ],
+    );
+
+    final repository = container.read(weatherRepositoryProvider);
+
     when(mock.fetchWeatherInfo()).thenReturn(response);
     expect(
-      api.getWeatherInfo(),
+      repository.getWeatherInfo(),
       const WeatherInfo(
         maxTemperature: 25,
         minTemperature: 7,
         weatherCondition: WeatherCondition.cloudy,
       ),
+    );
+  });
+
+  test('this is InvalidParameter test', () {
+    final mock = MockWeatherAPI();
+
+    final container = createContainer(
+      overrides: [
+        weatherAPIProvider.overrideWith((ref) {
+          return mock;
+        }),
+      ],
+    );
+
+    final repository = container.read(weatherRepositoryProvider);
+
+    when(mock.fetchWeatherInfo()).thenThrow(const InvalidParameter());
+    expect(
+      repository.getWeatherInfo,
+      throwsA(isA<InvalidParameter>()),
+    );
+  });
+
+  test('this is Unknown test', () {
+    final mock = MockWeatherAPI();
+
+    final container = createContainer(
+      overrides: [
+        weatherAPIProvider.overrideWith((ref) {
+          return mock;
+        }),
+      ],
+    );
+
+    final repository = container.read(weatherRepositoryProvider);
+
+    when(mock.fetchWeatherInfo()).thenThrow(const Unknown());
+    expect(
+      repository.getWeatherInfo,
+      throwsA(isA<Unknown>()),
     );
   });
 }
